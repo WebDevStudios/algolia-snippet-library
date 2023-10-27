4392b6c37739c501a9f6f7718d390dfa4cf0a049
Michael Beckwith <tw2113@gmail.com>
2023-10-27 12:39:50 -0500

    snippet for javascript to convert integer properties to currency strings

---
+++ instantsearch/currency-formatting.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * The code in this file is meant to be part of the instantsearch.php or autocomplete.php template files,
+ * most specifically in the javascript sections, to be used with template rendering.
+ */
+/*
+
+// Change to your currency and formatting, as applicable
+const dollars = new Intl.NumberFormat(`en-US`, {
+	currency: `USD`,
+	style   : 'currency',
+} );
+
+// hit.price value is an integer of say 5
+price = dollars.format( hit.price ); // console.log( price ) will show $5.00
+
+More information: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl
+ */
