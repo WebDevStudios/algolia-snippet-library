dabbe29e109da01fdbe89230146ee7a63d8d1ace
Michael Beckwith <tw2113@gmail.com>
2023-10-27 12:40:53 -0500

    external example of changing autocomplete sources based on where viewed within wordpress

---
+++ autocomplete-configuration/conditional-autocomplete-sources-by-content-type.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * This external resource shows how to change which Autocomplete indexes to use, depending on what type of content
+ * is currently being viewed. For example, only use "page" Autocomplete when viewing a page, else use other sources.
+ */
+
+// See: https://michaelbox.net/2022/10/change-algolia-autocomplete-sources-by-location-in-wordpress/
