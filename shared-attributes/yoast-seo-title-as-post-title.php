96d11a5672488c8676f7c53d7b54f7b5dbfaf1ad
Michael Beckwith <tw2113@gmail.com>
2023-10-27 10:49:43 -0500

    notes and function name

--- shared-attributes/yoast-seo-title-as-post-title.php
+++ shared-attributes/yoast-seo-title-as-post-title.php
@@ -1,5 +1,9 @@
 <?php
-function pluginize_yoast_seo_as_algolia_title( $shared_attributes, $post ) {
+/*
+ * This snippet can be used to fetch the Yoast SEO title and if set, override the default `post_title` value.
+ */
+
+function wds_algolia_yoast_seo_as_algolia_title( $shared_attributes, $post ) {
 	$maybe_seo_title = YoastSEO()->meta->for_post( $post->ID );
 
 	if ( false !== $maybe_seo_title ) {
@@ -8,6 +12,7 @@ function pluginize_yoast_seo_as_algolia_title( $shared_attributes, $post ) {
 
 	return $shared_attributes;
 }
-
-add_filter( 'algolia_post_shared_attributes', 'pluginize_yoast_seo_as_algolia_title', 10, 2 );
-add_filter( 'algolia_searchable_post_shared_attributes', 'pluginize_yoast_seo_as_algolia_title', 10, 2 );
+// For Search page/InstantSearch results.
+add_filter( 'algolia_searchable_post_shared_attributes', 'wds_algolia_yoast_seo_as_algolia_title', 10, 2 );
+//For Autocomplete results.
+add_filter( 'algolia_post_shared_attributes', 'wds_algolia_yoast_seo_as_algolia_title', 10, 2 );
